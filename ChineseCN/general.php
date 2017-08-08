4f1b4dde45175b2ae9e6306b33b65d38057b8489
hgc233 <779535192@qq.com>
2017-08-08 23:23:46 +0800

    更新汉化 general.php
    麦口管理层汉化

--- ChineseCN/general.php
+++ ChineseCN/general.php
@@ -13,80 +13,80 @@ $language = array(
 	/*
 	 *  General terms
 	 */ 
-	'submit' => 'Submit',
-	'register' => 'Register',
-	'sign_in' => 'Log In',
-	'log_out' => 'Log Out',
-	'profile' => 'Profile',
-	'invalid_token' => 'Invalid token, please try again.',
-	'close' => 'Close',
-	'back' => 'Back',
-	'warning' => 'Warning',
-	'date' => 'Date',
-	'search' => 'Search',
-	'edit' => 'Edit',
-	'next' => 'Next',
-	'confirm_cancel' => 'Are you sure you wish to cancel?',
-	'confirm_delete' => 'Confirm deletion',
-	'confirm_deletion' => 'Are you sure you wish to delete this?',
-	'cancel' => 'Cancel',
-	'delete' => 'Delete',
-	'none' => 'None',
-	'view' => 'View',
-	'change' => 'Change',
-	'remove' => 'Remove',
-	'info' => 'Info',
-	'confirm' => 'Confirm',
-	'actions' => 'Actions',
-	'alerts' => 'Alerts',
-	'view_alerts' => 'View alerts',
-	'no_alerts' => 'No alerts',
-	'x_new_alerts' => 'You have {x} new alerts.', // Don't replace {x}
-	'messages' => 'Messages',
-	'view_messages' => 'View messages',
-	'no_messages' => 'No messages',
-	'x_new_messages' => 'You have {x} new messages.', // Don't replace {x}
-	'loading' => 'Loading...',
-	'report' => 'Report',
-	'page_loaded_in' => 'Page loaded in {x}s', // Don't replace {x}, time unit is seconds
-    'page_x' => 'Page {x}',
-    'browse' => 'Browse',
+	'submit' => '提交',
+	'register' => '注册',
+	'sign_in' => '登入',
+	'log_out' => '登出',
+	'profile' => '个人资料',
+	'invalid_token' => '无效的令牌，请重试.',
+	'close' => '关闭',
+	'back' => '返回',
+	'warning' => '警告',
+	'date' => '日期',
+	'search' => '搜索',
+	'edit' => '编辑',
+	'next' => '下一个',
+	'confirm_cancel' => '你确定你要关闭吗?',
+	'confirm_delete' => '确认删除',
+	'confirm_deletion' => '你确定你要删除吗?',
+	'cancel' => '取消',
+	'delete' => '删除',
+	'none' => '没有',
+	'view' => '查看',
+	'change' => '更改',
+	'remove' => '移除',
+	'info' => '信息',
+	'confirm' => '确认',
+	'actions' => '操作',
+	'alerts' => '提醒',
+	'view_alerts' => '查看提醒',
+	'no_alerts' => '无提醒',
+	'x_new_alerts' => '你有 {x} 条新的提醒.', // Don't replace {x}
+	'messages' => '消息',
+	'view_messages' => '查看消息',
+	'no_messages' => '无消息',
+	'x_new_messages' => '你有 {x} 条新消息.', // Don't replace {x}
+	'loading' => '载入中...',
+	'report' => '报告',
+	'page_loaded_in' => '加载页面 {x}s', // Don't replace {x}, time unit is seconds
+    'page_x' => '页面 {x}',
+    'browse' => '浏览',
 
     /*
      *  Contact form
      */
-    'contact' => 'Contact',
-    'message' => 'Message',
-    'contact_email_subject' => 'New contact form submission',
-    'contact_message_failed' => 'Unable to send message. Please ensure you have entered a message between 10 and 5000 characters long.',
-    'contact_message_sent' => 'Message sent successfully.',
-    'contact_message_limit' => 'You can only send one message per hour. You can next send a message in {x} minutes.', // Don't replace {x}
+    'contact' => '联系',
+    'message' => '消息',
+    'contact_email_subject' => '新的联系表单提交',
+    'contact_message_failed' => '无法发送消息。 请确保您输入了10到5000个字符之间的消息.',
+    'contact_message_sent' => '消息已成功发送.',
+    'contact_message_limit' => '你每小时只能发送一条消息. 接下来你可以在 {x} 分钟内发送消息.', // Don't replace {x}
 	
 	/* 
 	 *  Navbar
 	 */
-	'home' => 'Home',
+	'home' => '主页',
 	
 	/*
 	 *  Homepage
 	 */
-	'social' => 'Social',
+	'social' => '社交',
 	
 	
 	/*
 	 *  Help dialogues
 	 */
-	'markdown_help' => 'You can format your post using Markdown. For more information, see <a href=\'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet\' target=\'_blank\'>here</a>.',
+	'markdown_help' => '您可以使用Markdown格式化您的帖子。 有关更多信息，请参阅 <a href=\'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet\' target=\'_blank\'>here</a>.',
 
     /*
      *  Server query
      */
-    'online' => 'Online',
-    'offline' => 'Offline',
+    'online' => '在线',
+    'offline' => '离线',
     'player_count' => '{x}/{y}', // Don't replace {x} or {y}
-    'ping' => 'Ping: {x}', // Don't replace {x}
-    'version' => 'Version: {x}', // Don't replace {x}
-    'currently_x_players_online' => 'There are currently <strong>{x}</strong> players online.', // Don't replace {x}
-    'server_offline' => 'The server is currently offline.',
-    'connect_with_ip_x' => 'Connect now using the IP <strong>{x}</strong>' // Don't replace {x}
+    'ping' => '延迟: {x}', // Don't replace {x}
+    'version' => '版本: {x}', // Don't replace {x}
+    'currently_x_players_online' => '目前有 <strong>{x}</strong> 玩家在线.', // Don't replace {x}
+    'server_offline' => '服务器目前处于离线状态.',
+    'connect_with_ip_x' => '使用 IP: <strong>{x}</strong> 来连接服务器' // Don't replace {x}
 );
