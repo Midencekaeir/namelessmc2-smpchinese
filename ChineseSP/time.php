8a82045531a043e69e93856e880b0b10ddabd392
Midencekaeir <alprenx@gmail.com>
2017-08-08 04:55:37 -0500

    增加原版EnglishUS文件
    本翻译将基于EnglishUS.

---
+++ ChineseSP/time.php
@@ -0,0 +1,29 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-dev
+ *
+ *  License: MIT
+ *
+ *  EnglishUS Language - Time
+ */
+
+$language = array(
+	/*
+	 *  Time
+	 */
+	'time' => 'time', // Placeholder, no need to translate
+	'seconds_short' => 's', // Shortened "seconds", eg "s"
+	'less_than_a_minute' => '数分钟之前',
+	'1_minute' => '一分钟之前',
+	'_minutes' => '{x}分钟之前',
+	'about_1_hour' => '大约一小时之前',
+	'_hours' => '{x}小时之前',
+	'1_day' => '一天之前',
+	'_days' => '{x}天之前',
+	'about_1_month' => '大约一个月之前',
+	'_months' => '{x}个月之前',
+	'about_1_year' => '大约一年之前',
+	'over_x_years' => '超过{x}年之前'
+);
