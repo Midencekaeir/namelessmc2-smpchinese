a4ef396eb12c3667ee1e90902238c62b7c1aedeb
Midencekaeir <alprenx@gmail.com>
2017-08-08 17:57:59 +0800

    修改language_html条目为zh

--- ChineseSP/version.php
+++ ChineseSP/version.php
@@ -1,8 +1,7 @@
 <?php 
 /*
- *	Made by Samerton
- *  https://github.com/NamelessMC/Nameless/
- *  NamelessMC version 2.0.0-pr2
+ *	Made by Midencekaeir
+ *  for NamelessMC version 2.0.0-pr2
  *
  *  License: MIT
  *
